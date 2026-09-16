3e5fe8ffeaca0119a30dd78d5e8a9144fcda16ac
Ari Maximiliano Muñiz Moncada <130394001+AriMMMoncada@users.noreply.github.com>
2026-09-16 16:07:33 -0600

    Update server.js

--- uniguide/backend/src/server.js
+++ uniguide/backend/src/server.js
@@ -30,7 +30,8 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
+      fontSrc: ["'self'", 'https://fonts.gstatic.com'],
       imgSrc: ["'self'", 'data:', 'https:'],
       frameSrc: ["'self'", 'https://www.google.com', 'https://maps.google.com'],
       connectSrc: ["'self'"],
